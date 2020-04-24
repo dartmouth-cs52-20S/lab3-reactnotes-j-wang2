4a17a995b4b49f56769c0efa0b2ef325c1f2b2fa
Jason Wang <jwang2@outlook.com>
2020-04-24 11:05:19 -0700

    addNote functionality

---
+++ src/components/addNote.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class AddNoteBtn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { title: '' };
+
+        // add to the bottom of your constructor
+        // this.onInputChange = this.onInputChange.bind(this);
+        // this.props = this.props.bind(this);
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    onInputChange = (event) => {
+        this.setState({ title: event.target.value });
+        console.log(event.target.value);
+    }
+
+    addNote = (e, data) => {
+        if (typeof this.props.onChange === 'function') {
+            this.props.addNote(e, data);
+        }
+    }
+
+    handleClick = () => {
+        console.log('this is the title');
+        console.log(this.state.title);
+        this.props.addNote(this.state.title);
+        console.log('clicked!');
+    }
+
+    render() {
+        return (
+            <div className="flex-container" id="add-note">
+                <input onChange={this.onInputChange} />
+                <button onClick={this.handleClick} type="button">Create new note</button>
+                <p> Title value: {this.state.title} </p>
+            </div>
+        );
+    }
+}
+
+export default AddNoteBtn;
---
+++ src/components/note.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import Draggable from 'react-draggable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.onClick = this.props.onClicke(this);d
+        console.log(props);
+        this.state = {
+            // title: '',
+            // text: '',
+            x: 0,
+            y: 0,
+            // zIndex: 0,
+        };
+    }
+
+    handleDeleteClick = (id) => {
+        this.setState((prevState) => ({
+            notes: prevState.notes.delete(id),
+        }));
+    }
+
+    handleDrag = (e, ui) => {
+        console.log('handlingdrag');
+        const { x, y } = this.state.deltaPosition;
+        this.setState({
+          deltaPosition: {
+            x: x + ui.deltaX,
+            y: y + ui.deltaY,
+          },
+        });
+    };
+
+    render() {
+        console.log('rendering');
+        console.log(this.props.id);
+        return (
+            <Draggable
+                handle="note"
+                grid={[25, 25]}
+                defaultPosition={{ x: 20, y: 20 }}
+                position={{
+                    x: this.state.x, y: this.state.y, width: 40, height: 40,
+                }}
+                onStart={this.handleStartDrag}
+                onDrag={this.handleDrag}
+                // onDrag={console.log('fuck me')}
+                onStop={this.handleStopDrag}
+            >
+                <div className="note">
+                    <h1> here we are!</h1>
+                    <h1> id={this.props.id} </h1>
+                    <h1> title={this.props.note.title} </h1>
+                    <FontAwesomeIcon onClick={this.handleDeleteClick} icon={faTrash} />
+                    <p> content={this.props.note.content} </p>
+                    <p>positionX={this.state.x} positionY={this.state.y}</p>
+                </div>
+            </Draggable>
+        );
+    }
+}
+
+export default Note;
